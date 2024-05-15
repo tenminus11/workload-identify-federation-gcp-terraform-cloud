c41bc1caf33a1c5d690bbacdebb60d939be39509
Jorge <50120389+jorgel272@users.noreply.github.com>
2024-05-15 15:24:15 +0200

    Initial test commit

--- use-wif/main.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  google_project_id = "example-project"
-}
-
-resource "google_storage_bucket" "example" {
-  name     = "example"
-  location = "EU"
-  project  = local.google_project_id
-}
